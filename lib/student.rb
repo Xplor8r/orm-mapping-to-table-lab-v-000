d5fb144daa0b745003bf584d0aa17d1186e2c155
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-27 23:56:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
     SQL
     
     DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT * FROM students ORDER BY id DESC LIMIT 1")
+    @id = DB[:conn].execute("SELECT * FROM students ORDER BY id DESC LIMIT 1").flatten[0]
   end
   
   def self.create(name = "", grade = "")
