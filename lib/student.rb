242c9541a08f91e9628a569eb65bdf3ceef41fa0
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-27 23:44:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,16 +13,15 @@ class Student
     @@all
   end
   
-  def self.create_table
-    sql = <<- SQL
-      CREATE TABLE IF NOT EXITS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
+ def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
         grade TEXT
-      )
-    SQL
-    
-    DB[:conn].execute(sql)
+        )
+        SQL
+    DB[:conn].execute(sql) 
   end
   
   def self.drop_table
